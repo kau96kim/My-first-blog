068fd0e58282365aeb2eacbb63eb6447b7825e9c
Jong <jhkim@babtalk.com>
2018-10-08 14:19:14 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v9#b%90_#_drf$ufzi5fw@@7p))dj+_hy=^bae&f+5jcwipd6%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
